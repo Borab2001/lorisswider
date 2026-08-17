9dfa25a6e3077a1ab45ed66eb9a8baa8d9b3b78c
Bora <borabalos9@gmail.com>
2026-08-17 23:55:05 +0200

    feat: CTA button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Button({ children, href }: { 
+    children: React.ReactNode;
+    href: string 
+}) {
+    return (
+        <Link
+            href={href}
+            className="inline-block min-w-45 bg-accent text-background font-medium font-sans px-8 py-3 text-xs md:text-lg hover:bg-accent/90 transition-colors"
+        >
+            {children}
+        </Link>
+    )
+}
