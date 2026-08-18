fd2dc9e8c542e51424555aef09af3f97e6d0f70d
Bora <borabalos9@gmail.com>
2026-08-18 19:58:39 +0200

    style: Button letter spacing adjustment

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,7 +7,7 @@ export default function Button({ children, href }: {
     return (
         <Link
             href={href}
-            className="inline-block min-w-45 bg-accent text-background font-medium font-sans px-8 py-3 text-base md:text-lg hover:bg-accent/90 transition-colors"
+            className="inline-block min-w-45 bg-accent text-background font-medium font-sans tracking-tight px-8 py-3 text-base md:text-lg hover:bg-accent/90 transition-colors"
         >
             {children}
         </Link>
